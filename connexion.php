595168016d2bd17d405aaaf77eea39a1c206cee9
Adrien Desmet <adrien.desmet@hotmail.fr>
2017-04-05 14:57:41 +0200

    Début du TP
    Création des fichiers : inscription.php, connexion.php et style.css.

---
+++ connexion.php
@@ -0,0 +1,34 @@
+<?php 
+
+	// Connexion à la base de données
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=cours_php;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));	
+	} catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion à l'espace membres</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="access_connexion">
+			<form action="connexion.php" method="POST">
+
+				<label for="pseudo">Pseudo</label>
+				<input type="texte" name="pseudo" id="pseudo" size="15"/><br/>
+
+				<label for="mot_de_passe">Mot de passe</label>
+				<input type="password" name="password" id="mot_de_passe" size="15"/><br/>
+
+				<input type="submit" name="envoyer"	value="Connexion"/><br/>
+
+				<a href="inscription.php" class="creation_compte">Créer un compte</a>
+			</form>
+		</div>
+	</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,52 @@
+<?php
+	// Connexion à la base de données
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=cours_php;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));	
+	} catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+	}
+
+	// Vérification du pseudo
+
+	if (isset($_POST['pseudo'])) {
+		
+		// vérification du pseudo dans la base de donneés
+		$reqPseudo = $bdd->prepare('SELECT pseudo FROM espace_membres WHERE pseudo = ?');
+		$reqPseudo->execute(array(htmlspecialchars($_POST['pseudo'])));
+		$verifPseudo = $reqPseudo->fetch();
+
+		if ($_POST['pseudo'] == $verifPseudo) {
+			echo "<p style=\"color: red;\">Ce pseudo existe déjà !</p>";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Inscription à l'espace membres</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="contenu_inscription">
+			<form action="inscription.php" method="POST">
+				<h3>Formulaire d'inscription à l'espace membres</h3>
+
+				<label class="lab_inscription" for="pseudo">Votre pseudo</label>
+				<input type="text" name="pseudo" required/><br/>
+
+				<label class="lab_inscription" for="email">Votre email</label>
+				<input type="email" name="email" required/><br/>
+
+				<label class="lab_inscription" for="password">Votre mot de passe</label>
+				<input type="password" name="password" required/><br/>
+
+				<label class="lab_inscription" for="passwordVerif">Vérification du mot de passe</label>
+				<input type="password" name="passwordVerif" required/><br/>
+				
+				<input type="submit" name="envoyer" value="Inscription"/>		
+			</form>
+		</div>
+	</body>
+</html>
